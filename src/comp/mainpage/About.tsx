23c5a03d22678ea91de274e09d110f78043e13ef
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-04-10 17:18:55 +0530

    fix bugs in about us page

--- src/comp/mainpage/About.tsx
+++ src/comp/mainpage/About.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Briefcase, Target, Clock, Users, CheckCircle, TrendingUp } from "lucide-react"
 import { motion, useInView } from "framer-motion"
 import { useRef } from "react"
